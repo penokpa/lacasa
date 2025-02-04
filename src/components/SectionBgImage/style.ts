2d143f2539b7136bb71504d3e4f65c6f91922411
penokpa <tasinima818@gmail.com>
2025-02-04 23:28:33 +0545

    changed our story to about us and slogan added

--- src/components/SectionBgImage/style.ts
+++ src/components/SectionBgImage/style.ts
@@ -3,10 +3,9 @@ import styled, { css } from "styled-components";
 export const StyledDiv = styled.div<{ $bgImage: string }>`
   ${({ $bgImage }) => css`
     &.section-image-wrapper {
-      background-image: url(${$bgImage});
+      background-image: url(https://images.unsplash.com/photo-1570598912132-0ba1dc952b7d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
       display: flex;
-      justify-content: center;
-      align-items: center;
+    //   align-items: center;
       flex-direction: column;
       padding: 250px 0;
       background-position: 50%;
@@ -34,4 +33,5 @@ export const StyledDiv = styled.div<{ $bgImage: string }>`
   `}
 `;
 
-//https://ucarecdn.com/77fa410d-bf68-4302-b095-f97271021eaf/-/progressive/yes/-/format/auto/-/resize/2000x/
+
+// https://ucarecdn.com/77fa410d-bf68-4302-b095-f97271021eaf/-/progressive/yes/-/format/auto/-/resize/2000x/
